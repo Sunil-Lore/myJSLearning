854379d230b290070a09a8e72d48c03b3c13eebc
Sunil Kumar <sunil.lore@outlook.com>
2026-06-10 18:52:25 +0530

    accessing and modifying object properties

---
+++ objects/object009.js
@@ -0,0 +1,7 @@
+let obj = { name: "Sourav", age: 23 };
+
+// Using Dot Notation
+console.log(obj.name);
+
+// Using Bracket Notation
+console.log(obj["age"]);
---
+++ objects/object010.js
@@ -0,0 +1,11 @@
+let obj = { name: "Sourav", age: 23 };
+
+// Using Dot Notation
+console.log(obj.name);
+obj.name = "Sourav Das";
+console.log(obj.name);
+
+// Using Bracket Notation
+console.log(obj["age"]);
+obj["age"] = 24;
+console.log(obj["age"]);
